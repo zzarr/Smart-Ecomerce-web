1cb57bb57b8630aae62276fb27c72878e5e038d2
ainii123 <ainiqurrotuaini2@gmail.com>
2024-05-28 13:23:02 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/js/app.js'])
-    <title>Document</title>
-</head>
-
-<body>
-
-
-
-</body>
-
-</html>
+@extends('layout.layout')
+@section('content')
+    
+@endsection
